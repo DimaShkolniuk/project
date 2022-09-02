624bc9f3f8eb780f35840e1df37892d502c017bb
Dima <MarkRais.16@gmail.com>
2022-09-02 19:01:22 +0300

    logika

--- js/script.js
+++ js/script.js
@@ -56,3 +56,73 @@ let incr = 10,
         console.log(isChecked || isClose);  // оператор ИЛИ
 
         console.log(isChecked ||  !isClose); //оператор отрецания "!"
+
+if (4 == 9) {  //всегда булиновое число true/false
+    console.log('Ok');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('So many');
+} else {
+    console.log('Ok');
+}
+
+//Условие       ответ если         Ответ если
+//которое       правда              ложь
+//выполнилось
+(num == 50) ? console.log('Ok') : console.log('So many');  // Тернарный оператор
+
+
+const num = 51;
+
+switch (num) { //проверка только сторого соответствия
+    case 49:
+        console.log('False');
+        break;
+    case 100:
+        console.log('false');
+        break;
+    case 50:
+        console.log('True');
+        break;
+    default: 
+        console.log("you must try");
+        break;
+}
+
+// И запинается на ЛЖИ. Логичесий элемент && выводит либо первую ложь до каторой
+// он доходит либо выводит значение последней правды которую он сравнивал.
+
+const hamburger = 5;
+const fries = 0;
+
+if (hamburger && fries) {
+    console.log('Nice');
+}
+
+console.log(hamburger && fries);
+
+
+const hamburger = 3;
+const fries = 1;
+const cola = 1;
+
+console.log(hamburger === 3 && cola && fries);
+
+console.log(1 && 0);
+console.log(1 && 5);
+console.log(null && 5);
+console.log(0 && 'wwww');
+
+if (hamburger === 3 && cola === 1 && fries) {
+    console.log('Nice');
+} else {
+    console.log('We go away');
+}
+
