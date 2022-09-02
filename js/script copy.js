588541d0621688b0fb85ca4b37d2e3dd8b335ce2
Dima <MarkRais.16@gmail.com>
2022-09-02 16:55:50 +0300

    First lessons

--- js/script copy.js
+++ js/script copy.js
@@ -1,28 +1,22 @@
-const arr = ['a', 'b', 'c'];
+"use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
-
-const arrObj = {
-    0: 'a',
-    1: 'b',
-    2: 'c',
-    abc: {
-        def:{
-
-        }
-    }
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-const b = 'b';
 
-//arrObj.b = '1234';
-arrObj[b] = '1234';
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
 
-console.log(arrObj['b']);
-console.log(arrObj.b);
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
 
-const obj = {
-    Anna: 500,
-    'Alice': 800
-};
+    console.log(personalMovieDB);
 
--- js/script.js
+++ js/script.js
@@ -16,9 +16,43 @@
 
 //console.log(typeof(answers));
 
-const category = 'toys';
+//const category = 'toys';
 
-console.log(`https://someurl.com/${category}/5`);
+//console.log(`https://someurl.com/${category}/5`);
 
-const user = "Dima";
-alert(`Hello, ${user}`);
+//const user = "Dima";
+//alert(`Hello, ${user}`);
+
+console.log('arr' + " - oject");
+console.log(4 + " - oject");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+    //incr++;
+    //decr--;
+
+    //console.log(incr);
+    //console.log(decr);
+
+    //console.log(incr++);  //Постфиксная форма
+    //console.log(decr--);  //Постфиксная форма
+
+    console.log(++incr);  //Префиксная форма
+    console.log(--decr);  //Префиксная форма
+
+    console.log(5%2);  //остаточное значение
+
+    console.log(2*4 == '8'); //сравнение
+
+    console.log(2 + 2 * 2 === 8);  //строгое сравнение
+    console.log(2 + 2 * 2 !== 8);  // не равенство
+
+    const isChecked = true;
+        isClose = false;     
+        console.log(isChecked && isClose);  // оператор И
+
+        console.log(isChecked || isClose);  // оператор ИЛИ
+
+        console.log(isChecked ||  !isClose); //оператор отрецания "!"
