27fc6f8f7eb0a19abb98108ee3efadd4f8ca7e22
Dima <MarkRais.16@gmail.com>
2022-09-08 21:08:38 +0300

    lessons 7

--- js/script copy.js
+++ js/script copy.js
@@ -10,13 +10,171 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+     const a = prompt('Один из последних просмотренных фильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != " " && b != ' ' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+         
+}   
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+    
 
     console.log(personalMovieDB);
 
+
+//обьявляем функция
+//function - говорит нам что это функция
+//showFirstMessage - даем ей название (Формат имени: Глагол + то над чем выполняется действие)
+//() - передаем аргументы функции
+//{} - прописываем действия которые будут выполнятся
+
+function showFirstMessage (text) {
+    console.log(text);
+    let num = 20;
+}
+
+showFirstMessage('Hello world');
+
+function ret() {
+    let num = 50;
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+    console.log('Hello');
+};
+
+logger();
+
+const calc = (a, b) => { return a + b};
+
+//1
+
+const usdCurr = 28;
+const eurCurr = 32;
+const discont = 0.9;
+
+function convert(amount, curr) {
+    console.log(curr * amount);
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discont);
+}
+const res = convert(500, usdCurr);
+promotion(res);
+
+promotion(convert(500, usdCurr));
+
+convert (500, usdCurr);
+convert (500, eurCurr);
+
+text ();
+
+function doNothing() {};
+console.log(doNothing() === undefined);
+
+//
+
+function sayHello(name) {
+    return `Привет, ${name}!`;
+    
+}
+sayHello ("Антон");
+console.log(sayHello ("Антон"));
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+returnNeighboringNumbers (5);
+
+console.log(returnNeighboringNumbers(5));
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    } 
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+        } else {
+            str += `${num * i}---`;
+        }
+    }
+
+    return str;
+}
+getMathResult(5, 3);
+getMathResult(3, 10); 
+getMathResult(10, 5);
+getMathResult(10, '5'); 
+getMathResult(10, 0);
+getMathResult(20, -5);
+
+console.log(getMathResult(5, 3));
+console.log(getMathResult(3, 10));
+console.log(getMathResult(10, 5));
+console.log(getMathResult(10, '5'));
+console.log(getMathResult(10, 0));
+console.log(getMathResult(20, -5));
+
+let a = 5;
+let b = 10;
+
+function multiply(a, b){
+    return (a * b);
+    
+  }
+  multiply(a, b);
+  console.log(multiply(a, b));
+
+
+
+const str = 'test';
+
+console.log(str.length); // свойство подсчет длины строки
+
+console.log(str.toUpperCase()); // метод - Пропись заглавными
+console.log(str.toLowerCase()); // метод - Пропись маленькими
+
+let fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit')); // метод определения данних внутри страки
+
+const logg = 'hello world';
+
+console.log(logg.slice(6, 11)); // метод вырезания из строки
+console.log(logg.substring(6, 11)); // метод 
+console.log(logg.substr(0, 5)); // метод сколько символов нужно вырезать
+
+const num = 12.2;
+
+console.log(Math.round(num)); //округление чисел
+
+const test = '12.2px';
+console.log(parseInt(test)); // изменение системы исчисления(в число) и округляет его
+console.log(parseFloat(test)); // изменение системы исчисления с плавающей точкой
--- js/script.js
+++ js/script.js
@@ -76,12 +76,13 @@ if (num < 49) {
 //Условие       ответ если         Ответ если
 //которое       правда              ложь
 //выполнилось
+const num = 51;
 (num == 50) ? console.log('Ok') : console.log('So many');  // Тернарный оператор
 
 
 const num = 51;
 
-switch (num) { //проверка только сторого соответствия
+switch (num) { //проверка только строгого соответствия
     case 49:
         console.log('False');
         break;
@@ -156,3 +157,234 @@ console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 //опервтор НЕ (!) 
 
 console.log(!0);
+
+console.log( NaN || 2 || undefined ); //2
+console.log( NaN && 2 && undefined ); //NaN
+console.log( 1 && 2 && 3 ); //3
+console.log( !1 && 2 || !3 ); //false
+console.log( 25 || null && !3 ); //25
+console.log( NaN || null || !3 || undefined || 5); //5
+console.log( NaN || null && !3 && undefined || 5); //5
+console.log( 5 === 5 && 3 > 1 || 5); //true
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+} //Done
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!');
+}
+console.log((hamburger || cola || fries === 3 || nuggets)); //2
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+}
+console.log((hamburger && cola || fries === 3 && nuggets)); //false
+
+
+//цикл 1!
+let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+//цикл 2!
+let num = 50;
+
+do{
+    console.log(num);
+    num++;
+}
+while (num <= 55);
+
+
+//цикл 3!
+let num = 50;
+// (начало цикла; условия остановки цикла; шаг цикла)
+for (let i = 1; i < 10; i++) {
+    if(i == 6) {
+        //break; //остановиться
+        continue; //исключить значение из цикла
+    }
+    console.log(i);
+    
+}
+
+//цикл в цикле (23)
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+// задача вложенного цыла
+
+let result = '';
+const length = 7;
+
+for(let i = 1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+}
+
+console.timeLog(result);
+
+//Полная остановка цикла
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if (k === 2) break first;
+            console.log(`Thirt level: ${k}`);
+        }
+    }
+}
+//1
+for (let i = 1; i < 101; i++) {
+    console.log(i);
+}
+
+let num = 0;
+while (num < 101){
+    console.log(num);
+    num++;
+}
+
+//2
+for (let i = 11; i < 34; i++) {
+    console.log(i);
+}
+
+let num = 11;
+while (num < 34) {
+    console.log(num);
+    num++;
+}
+//3
+
+for (count = 1; count < 100; count++) {
+    count++;
+    console.log(count);
+}
+
+let num = 0;
+while(num < 100) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+    num++;
+}
+
+//4
+let sum = 0;
+for (var i = 0; i <= 100; i++) {
+    sum += i;
+ }
+console.log(sum);
+
+let sum = 0;
+let num = 0;
+
+while (num <= 100) {
+    sum += num;
+    num++;
+}
+console.log(sum);
+
+//5
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
+}
+
+console.log(arrayOfNumbers);
+
+const arrayOfNumbers = [];
+let i = 5;
+while (i < 11) {
+    arrayOfNumbers[i - 5] = i;
+    i++;
+}
+console.log(arrayOfNumbers);
+
+//6
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (i = 0; i < arr.length; i++) {
+    result [i] = arr [i];
+}
+console.log(result);
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+let i = 0;
+
+while (i < arr.length) {
+    result[i] = arr [i];
+    i++;
+}
+console.log(result);
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i]) === 'string') {
+        data[i] = `${data[i]} - done`;
+    }
+}
+console.log(data);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+for (i = 1; i <= data.length; i++) {
+   result [i - 1] = data[data.length - i];
+}
+console.log(result);
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+console.log(result);
